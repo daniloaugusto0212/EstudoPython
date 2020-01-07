fc2e61176d5afb4650b8a30f76540a0cae54d2be
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-01-07 13:27:45 -0300

    Criando Sistema Para Pizzaria

--- novo/Python-Udemy/SistemaERP.py
+++ novo/Python-Udemy/SistemaERP.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pymysql.cursors
 
 conexao = pymysql.connect(
@@ -70,6 +71,117 @@ def cadastrarProdutos():
     except:
         print('Erro ao inserir dados!')
 
+def listarProdutos():
+    produtos = []
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from produtos')
+            produtosCadastrados = cursor.fetchall()
+
+    except:
+        print('Erro ao conectar ao banco de dados')
+
+    for i in produtosCadastrados:
+        produtos.append(i)
+
+    if len(produtos) != 0:
+        for i in range(0, len(produtos)):
+            print(produtos[i])
+    else:
+        print('Nenhum produto cadastrado!')
+
+def excluirProdutos():
+    idDeletar = int(input('Digite o ID referente ao produto que deseja apagar: \n'))
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('delete from produtos where id = {}'.format(idDeletar))
+            print('Produto {} excluído com sucesso!')
+    except:
+        print('Erro ao excluir o produto!')
+
+def listarPedidos():
+    pedidos = []
+    decision = 0
+
+    while decision != 2:
+        pedidos.clear()
+
+        try:
+            with conexao.cursor() as cursor:
+                cursor.execute('select * from pedidos')
+                listaPedidos = cursor.fetchall()
+        except:
+            print('Erro no banco de dados! ')
+
+        for i in listaPedidos:
+            pedidos.append(i)
+
+        if len(pedidos) != 0:
+            for i in range(0, len(pedidos)):
+                print(pedidos[i])
+        else:
+            print('Não há pedidos! ')
+
+        decision = int(input('[1] Produto entregue\n[2] Voltar\n'))
+
+        if decision == 1:
+            idDeletar = int(input('Digite o ID do pedido entregue: '))
+
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('delete from pedidos where id = {}'.format(idDeletar))
+                    print('Produto entregue.')
+            except:
+                print('Erro ao dar baixa no pedido! ')
+
+def gerarEstatistica():
+    nomeProdutos = []
+    nomeProdutos.clear()
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from produtos')
+            produtos = cursor.fetchall()
+    except:
+        print('Erro ao consultar o banco de dados. ')
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from estatiticaVendido')
+            vendido = cursor.fetchall()
+    except:
+        print('Error ao consultar o banco de dados. ')
+
+    estado = int(input('[0] SAIR\n[1] PESQUISAR POR NOME\n[2] PESQUISAR POR GRUPO\n'))
+
+    if estado == 1:
+        decisao3 = int(input('[1] PESQUISAR POR VALOR\n[2] PESQUISAR POR QUANTIDADE\n'))
+        if decisao3 == 1:
+
+            for i in produtos:
+                nomeProdutos.append(i['nome'])
+
+            valores = []
+            valores.clear()
+
+            for h in range(0, len(nomeProdutos)):
+                somaValor = -1
+                for i in vendido:
+                    if i['nome'] == nomeProdutos[h]:
+                        somaValor += i['preco']
+                if somaValor == -1:
+                    valores.append(0)
+                elif somaValor > 0:
+                    valores.append(somaValor + 1)
+
+            plt.plot(nomeProdutos, valores)
+            plt.ylabel('Quantidade vendida em Reais')
+            plt.xlabel('Produtos')
+            plt.show()
+
+
 while not autentico:
     decisao = int(input('Digite:\n[1] LOGAR\n[2]CADASTRAR\n'))
 
@@ -92,7 +204,18 @@ if autentico == True:
 
 
         while decisaoUsuario != 0:
-            decisaoUsuario = int(input('[1] CADASTRAR\n[0] SAIR\n'))
+            decisaoUsuario = int(input('[0] SAIR\n[1] CADASTRAR\n[2] LISTAR PRODUTOS CADASTRADOS\n[3] LISTAR PEDIDOS\n[4] VISUALIZAR ESTATISTICAS\n'))
 
             if decisaoUsuario == 1:
                 cadastrarProdutos()
+            elif decisaoUsuario == 2:
+                listarProdutos()
+
+                delete = int(input('[1] Excluir um produto\n[2] Sair\n'))
+                if delete == 1:
+                    excluirProdutos()
+            elif decisaoUsuario == 3:
+                listarPedidos()
+            elif decisaoUsuario == 4:
+                gerarEstatistica()
+
