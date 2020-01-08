7e751e1ebe6fc158f234d48b652ea23a7fe6b3f1
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-01-08 20:07:34 -0300

    Editando Janelas

--- novo/Python-Udemy/ERPInterface.py
+++ novo/Python-Udemy/ERPInterface.py
@@ -2,7 +2,9 @@ from tkinter import *
 
 janela = Tk()
 janela.title('Curso de Python')
-janela.geometry('800x300')
+janela.geometry('500x500')
 janela.resizable(False, False)
+
+Label(janela, text='Hello World', bg='black', fg='white', padx=30, pady=30).grid(row=0, column=0)
 
 janela.mainloop()
