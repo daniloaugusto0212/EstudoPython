885d95f8d470e2e6df6204de12ad67f95088696f
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-01-23 17:12:24 -0300

    Adicionando exercício

---
+++ exercicios/PythonGuanabara/ex027PrimeiroEUltimoNome.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+n = nome.split()
+print('Muito prazer em te conhecer!')
+print(f'Seu primeiro nome é {n[0]}')
+print(f'Seu último nome é {n[-1]}')
