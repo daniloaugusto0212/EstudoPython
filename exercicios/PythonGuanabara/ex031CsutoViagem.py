ebf93e73b23424c5f07016c98d4ca84fcc298b6c
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-01-23 17:48:41 -0300

    Adicionando exercício

--- exercicios/PythonGuanabara/ex031CsutoViagem.py
+++ exercicios/PythonGuanabara/ex031CsutoViagem.py
@@ -1,7 +1,8 @@
 distancia = float(input('Qual é a distância da sua viagem? '))
 print(f'Você está prestes a começar uma viagem de {distancia:.1f}Km.')
-if distancia <= 200:
+'''if distancia <= 200:
     preco = distancia * 0.5
 else:
-    preco = distancia * 0.45
+    preco = distancia * 0.45'''
+preco = 0.5 * distancia if distancia <= 200 else distancia * 0.45 #Usando if ternário
 print(f'E o preço de sua passagem será de R${preco:.2f}')
