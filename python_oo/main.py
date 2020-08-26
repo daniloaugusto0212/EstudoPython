45ce53399a9a283bd8ec0cc79a3b834a053bbb30
Danil Augusto <daniloaugusto0212@hotmail.com>
2020-08-25 21:29:49 -0300

    Docstring

--- python_oo/main.py
+++ python_oo/main.py
@@ -1,7 +1,9 @@
 import carro
 
 uno_vermelho = carro.Carro("Vermelho", 4, "Flex", 1.0, 0, False, 0)
+help(uno_vermelho.abastecer) #Imprime o conteúdo da DocString
 uno_vermelho.ligar()
+help(carro.Carro)
 uno_vermelho.abastecer(10)
 uno_vermelho.abastecer(20)
 
@@ -12,5 +14,5 @@ uno_preto= carro.Carro("Preto", 2, "Flex", 1.4, 0, False, 0)
 uno_preto.ligar()
 
 print(f'\nA quantidade de combustível do Uno Preto é {uno_preto.qtd_combustivel} litros')
-uno_preto.acelerar(100)
+uno_preto.acelerar(120)
 print(f'\nO carro está na velocidade de {uno_preto.velocidade} Km/h')
