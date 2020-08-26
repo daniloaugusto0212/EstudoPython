1382c8c881709e4f24449a3821ab1aa37b13f3c1
Danil Augusto <daniloaugusto0212@hotmail.com>
2020-08-25 21:11:40 -0300

    Método que recebem argumentos

--- python_oo/main.py
+++ python_oo/main.py
@@ -1,12 +1,16 @@
 import carro
 
-uno_vermelho = carro.Carro("Vermelho", 4, "Flex", 1.0, 0, False)
+uno_vermelho = carro.Carro("Vermelho", 4, "Flex", 1.0, 0, False, 0)
 uno_vermelho.ligar()
-uno_vermelho.abastecer()
-uno_vermelho.abastecer()
+uno_vermelho.abastecer(10)
+uno_vermelho.abastecer(20)
+
 print(f'A quantidade de combustível do carro é {uno_vermelho.qtd_combustivel} litros\n')
 
 
-uno_preto= carro.Carro("Preto", 2, "Flex", 1.4, 0, True)
-uno_preto.desligar()
+uno_preto= carro.Carro("Preto", 2, "Flex", 1.4, 0, False, 0)
+uno_preto.ligar()
+
 print(f'\nA quantidade de combustível do Uno Preto é {uno_preto.qtd_combustivel} litros')
+uno_preto.acelerar(100)
+print(f'\nO carro está na velocidade de {uno_preto.velocidade} Km/h')
