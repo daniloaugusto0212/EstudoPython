566d4e69b53dcc147c63f82280b9e4ee12ac9b33
Danil Augusto <daniloaugusto0212@hotmail.com>
2020-08-27 20:05:12 -0300

    Conceito de Polimorfismo

--- python_oo/main.py
+++ python_oo/main.py
@@ -1,9 +1,9 @@
 import carro, moto
 
 uno_vermelho = carro.Carro("Vermelho", "Flex", 1.0, 4)
-help(uno_vermelho.abastecer) #Imprime o conteúdo da DocString
+#help(uno_vermelho.abastecer) #Imprime o conteúdo da DocString
 uno_vermelho.ligar()
-help(carro.Carro)
+#help(carro.Carro)
 uno_vermelho.abastecer(10)
 uno_vermelho.abastecer(20)
 print(f'A quantidade de combustível do carro é {uno_vermelho.qtd_combustivel} litros\n')
@@ -11,5 +11,6 @@ print(f'A quantidade de combustível do carro é {uno_vermelho.qtd_combustivel} 
 
 moto_preta = moto.Moto("Vermelha", "Gasolina", 125, 2)
 moto_preta.abastecer(30)
+moto_preta.abastecer((10))
 moto_preta.ligar()
 print(moto_preta.is_ligado)
