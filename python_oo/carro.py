ad49827a52b1031ee88ecf5ba1c8394ce2a1773a
Danil Augusto <daniloaugusto0212@hotmail.com>
2020-08-27 19:19:28 -0300

    Construtores - PErsonalizando o método __init__

--- python_oo/carro.py
+++ python_oo/carro.py
@@ -2,25 +2,28 @@ class Carro():
     """"
     Essa é a classe carro. Esta classe é utilizada para instanciar novos carrso em nosso programa
     """
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
-        self.qtd_combustivel = qtd_combustivel
-        self.is_ligado = is_ligado
-        self.velocidade = velocidade
+        self.qtd_combustivel = 0
+        self.is_ligado = False
+        self.velocidade = 0
 
     def abastecer(self, qtd_combustivel):
         """"O método abastecer recebe como parâmetro a quantidade de combustível e incrementa no atributo qtd_combustível"""
         self.qtd_combustivel += qtd_combustivel
 
     def ligar(self):
-        if self.is_ligado == False:
-            self.is_ligado = True
-            print("O carro foi ligado!")
+        if self.qtd_combustivel <= 0:
+            print('O carro não possui combustível, impossível ligar!')
         else:
-            print("O carro já está ligado!")
+            if self.is_ligado == False:
+                self.is_ligado = True
+                print("O carro foi ligado!")
+            else:
+                print("O carro já está ligado!")
 
     def desligar(self):
         if self.is_ligado:
