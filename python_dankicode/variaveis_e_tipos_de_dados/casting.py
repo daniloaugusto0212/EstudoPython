3d2207914c871c6d3d1919ae59f8e9becf404c23
Danil Augusto <daniloaugusto0212@hotmail.com>
2021-02-25 18:13:02 -0300

    variaveis e tipos de dados

---
+++ python_dankicode/variaveis_e_tipos_de_dados/casting.py
@@ -0,0 +1,26 @@
+#int
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+#float
+
+a = float(2.3)
+b = float(2)
+c = float('2.3')
+d = float('2')
+
+print(a, b, c, d)
+
+#string
+
+t = str('s1')
+r = str(2)
+z = str(2.3)
+
+print("A variavel t eh do tipo: ", type(t));
+print("A variavel t eh do tipo: ", type(r));
+print("A variavel t eh do tipo: ", type(z));
---
+++ python_dankicode/variaveis_e_tipos_de_dados/escopo_variaveis.py
@@ -0,0 +1,19 @@
+#escopo de variaveis
+
+"""
+as variaveis globais
+
+as variaveis locais
+
+"""
+
+x = 5
+
+def funcao():
+    x = 3
+    print("Valor da variavel local: ", x)
+
+funcao()
+
+print("Valor da variavel global: ", x)
+
