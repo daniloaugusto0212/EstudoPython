2c3e4b523848cca946fc4fcc0946f3f45cd1f4ac
Danilo <daniloaugusto0212@hotmail.com>
2021-03-09 20:53:33 -0300

    Iniciando estudo sobre tuplas

---
+++ python_dankicode/tuplas/tuplas.py
@@ -0,0 +1,12 @@
+lista = ['item1', 'item2', 'item3']
+print(lista)
+print(len(lista))
+print(type(lista))
+
+print("-------"*10)
+
+#tuplas são imutáveis
+tupla = ("item1", "item2", "item3")
+print(tupla)
+print(len(tupla))
+print(type(tupla))
